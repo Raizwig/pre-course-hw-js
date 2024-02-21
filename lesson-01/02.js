4a6b8b9f4ac08d6a76ee1e225be60078d6407709
Raizwig <105453771+Raizwig@users.noreply.github.com>
2024-02-21 22:21:18 +0300

    first commit

---
+++ lesson-01/02.js
@@ -0,0 +1,7 @@
+// code
+let number1 = Math.floor(Math.random() * 100);
+if (number1 % 2 !== 0) {
+  console.log(`${number1} Это число не чётное`);
+} else {
+  console.log(`${number1} Это число чётное`);
+}
