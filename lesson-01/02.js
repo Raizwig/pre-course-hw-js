5ca6a1682789cceb2898805427312154ab9b202a
Raizwig <105453771+Raizwig@users.noreply.github.com>
2024-03-31 21:52:36 +0300

    7е исправление

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -1,6 +1,4 @@
 let number1 = Math.floor(Math.random() * 100);
-if (number1 % 2 === 0) {
-  console.log(`${number1}: Это число чётное`);
-} else {
-  console.log(`${number1}: Это число не чётное`);
+if (number1 % 2 === 0){console.log(`${number1}:Это число чётное`);
+}else{console.log(`${number1}:Это число нечётное`);
 }
