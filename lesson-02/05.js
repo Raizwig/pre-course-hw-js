e8d5e20002f273039d8be3b3ef7676e8f0a457cf
Raizwig <105453771+Raizwig@users.noreply.github.com>
2024-04-04 23:05:19 +0300

    5е задание

---
+++ lesson-02/05.js
@@ -0,0 +1,9 @@
+let passport = {
+  name: "Petr",
+  surname: "Petrov",
+};
+let copyPassport = Object.assign({}, passport);
+copyPassport.name = "Ivan";
+console.log(passport.name);
+console.log(copyPassport.name);
+
