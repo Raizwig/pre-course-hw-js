8c2ae98ca48ccf9a4fb705d427e9a5bfb9b8c141
Raizwig <105453771+Raizwig@users.noreply.github.com>
2024-04-04 21:45:07 +0300

    1е задание 2е исправление

--- lesson-02/01.js
+++ lesson-02/01.js
@@ -1,6 +1,2 @@
-// function doArrow(value) {
-//   console.log(value);
-// }
-// doArrow("Передай 5");
-let doArrow = (value) => console.log(value);
+let doArrow = value => console.log(value);
 doArrow("Передай 5");
