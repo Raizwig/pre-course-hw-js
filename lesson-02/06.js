7b08001e4ad3401eb980230acd3515acbdcffd19
Raizwig <105453771+Raizwig@users.noreply.github.com>
2024-04-05 00:23:24 +0300

    6е задание 1 исправление

--- lesson-02/06.js
+++ lesson-02/06.js
@@ -7,6 +7,6 @@ let passportWithAddress = {
   },
 };
 console.log(passportWithAddress.address.city);
-let copyPassportWithAddress = structuredClone(passportWithAddress, {address: city});
+let copyPassportWithAddress = structuredClone(passportWithAddress);
 copyPassportWithAddress.address.city = "Bobryisk";
-console.log(copyPassportWithAddress.address.city);
+console.log(copyPassportWithAddress);
