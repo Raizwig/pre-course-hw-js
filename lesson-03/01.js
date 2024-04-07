d8087855a90e4ad424a0f5155384e52acf9cd882
Raizwig <105453771+Raizwig@users.noreply.github.com>
2024-04-07 09:16:24 +0300

    1е задание

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,1 +1,10 @@
-// code
+const colors = ["синий", "черный", "зеленый", "красный", "желтый", "зеленый"];
+function createColorString() {
+  let createColors = colors.filter(function (element) {
+    return (
+      element === "черный" || element === "красный" || element === "желтый"
+    );
+  });
+  return createColors.join("-");
+}
+console.log(createColorString());
